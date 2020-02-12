dbfe1cd5d6b3ab89db8d32f7e56118b251aabd49
guillem <guillem.cordoba@gmail.com>
2020-02-12 14:25:36 -0500

    Updated example

--- example-dna/zomes/example/code/src/lib.rs
+++ example-dna/zomes/example/code/src/lib.rs
@@ -12,18 +12,14 @@ use hdk::{entry_definition::ValidatingEntryType, error::ZomeApiResult};
 
 use hdk::holochain_persistence_api::cas::content::Address;
 
-use hc_roles_mixin;
 use hdk_proc_macros::zome;
-
-use hc_roles_mixin::Role;
+use holochain_roles;
 
 #[zome]
 mod my_zome {
 
     #[init]
     fn init() {
-        hc_roles_mixin::handlers::create_admin_role()?;
-
         Ok(())
     }
 
@@ -34,7 +30,7 @@ mod my_zome {
 
     #[entry_def]
     fn role_entry_def() -> ValidatingEntryType {
-        hc_roles_mixin::role_entry_def()
+        holochain_roles::role_entry_def()
     }
 
     #[entry_def]
@@ -43,32 +39,22 @@ mod my_zome {
     }
 
     #[zome_fn("hc_public")]
-    fn create_role(role_name: String) -> ZomeApiResult<Address> {
-        hc_roles_mixin::handlers::create_role(&role_name)
-    }
-
-    #[zome_fn("hc_public")]
     fn assign_role(role_name: String, agent_address: Address) -> ZomeApiResult<()> {
-        hc_roles_mixin::handlers::assign_role(&role_name, &agent_address)
+        holochain_roles::handlers::assign_role(&role_name, &agent_address)
     }
 
     #[zome_fn("hc_public")]
     fn unassign_role(role_name: String, agent_address: Address) -> ZomeApiResult<()> {
-        hc_roles_mixin::handlers::unassign_role(&role_name, &agent_address)
+        holochain_roles::handlers::unassign_role(&role_name, &agent_address)
     }
 
     #[zome_fn("hc_public")]
-    fn get_role(role_name: String) -> ZomeApiResult<Role> {
-        hc_roles_mixin::handlers::get_role(&role_name)
+    fn get_role_agents(role_name: String) -> ZomeApiResult<Role> {
+        holochain_roles::handlers::get_role_agents(&role_name)
     }
 
     #[zome_fn("hc_public")]
-    fn get_all_roles() -> ZomeApiResult<Vec<Role>> {
-        hc_roles_mixin::handlers::get_all_roles()
-    }
-
-    #[zome_fn("hc_public")]
-    fn get_agent_roles(agent_address: Address) -> ZomeApiResult<Vec<Role>> {
+    fn get_agent_roles(agent_address: Address) -> ZomeApiResult<Vec<String>> {
         hc_roles_mixin::handlers::get_agent_roles(&agent_address)
     }
 }
